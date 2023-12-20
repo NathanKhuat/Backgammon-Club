76c6455bbb948f8342305afa6c1e8ccae7abab22
Dhreeti Rathore <rdhre1@Dhreetis-MBP.lan>
2023-12-20 01:53:46 -0800

    draft meeting section

--- backgammon/src/components/MeetingSection/MeetingSection.tsx
+++ backgammon/src/components/MeetingSection/MeetingSection.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
 import './MeetingSection.css';
+import dice from './dice.png'
 
 function MeetingSection() {
   return (
-    <div>
-      MeetingSection
+    <div className='wrapper'>
+      <div className='text'>
+        <h2 className='header-text'>Meetings</h2>
+        <span className='meeting-text'>
+          Weekly Meetings on Tuesdays <br/>
+          Big Time Brewery @5 <br/>
+          Quarterly tournaments @ somewhere</span>
+      </div>
+      <div className='media'>
+        <img src={dice} className="img"></img>
+        <button className="btn">Join our Discord!</button>
+      </div>
     </div>
   );
 }
--- backgammon/src/components/WinnerSection/WinnerSection.tsx
+++ backgammon/src/components/WinnerSection/WinnerSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './WinnerSection.css';
 
 function WinnerSection() {
