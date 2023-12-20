43a0f3245f87d5f948b5aa979029fc9ea334e016
Nathan Khuat <nkhuat@cs.washington.edu>
2023-12-20 15:01:07 -0800

    roughly finished the welcome and nav bar

--- backgammon/src/components/Navbar/Navbar.tsx
+++ backgammon/src/components/Navbar/Navbar.tsx
@@ -2,8 +2,13 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className='nav-wrapper'>
+      <span className='title-text'>UW Backgammon</span>
+      <div className='nav-options'>
+        <button className='nav-button'>Home</button>
+        <button className='nav-button'>Meetings</button>
+        <button className='nav-button'>About</button>
+      </div>
     </div>
   );
 }
--- backgammon/src/components/WelcomeSection/WelcomeSection.tsx
+++ backgammon/src/components/WelcomeSection/WelcomeSection.tsx
@@ -1,9 +1,15 @@
 import './WelcomeSection.css';
+import board from './board2.png'
 
 function WelcomeSection() {
   return (
-    <div>
-      Welcome Section
+    <div className='welcome-wrapper'>
+      <div className='welcome-text'><span>
+        University of Washington's <br/>
+        Undergraduate <br/>
+        Backgammon Club</span></div>
+      <div className='welcome-cover'></div>
+      <img src={board} className='welcome-img' />
     </div>
   );
 }
