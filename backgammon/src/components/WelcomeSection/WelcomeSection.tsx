b85f51cd88512dba8054285d93e90f2380713d93
Nathan Khuat <nkhuat@cs.washington.edu>
2023-12-25 17:00:36 -0800

    finished majority animations

--- backgammon/src/components/WelcomeSection/WelcomeSection.tsx
+++ backgammon/src/components/WelcomeSection/WelcomeSection.tsx
@@ -9,20 +9,20 @@ function WelcomeSection() {
         Undergraduate <br/>
         Backgammon Club
       </span>
-      <div className='welcome-cover'>
+      <div className='welcome-cover trigger1 animate fadeIn'>
       </div>
-      <img src={board} className='welcome-img' />
-      <div className='about1'></div>
-      <span className='about-text trigger1 animate fadeIn'>About</span>
-      <div className='about-bar trigger1 animate fadeIn'></div>
-      <span className='about-message trigger1 animate fadeIn'>
+      <img src={board} className='welcome-img trigger3 animate fadeIn' />
+      <div className='about1 trigger4 animate fadeLeftSpecial'></div>
+      <span className='about-text trigger5 animate fadeIn'>About</span>
+      <div className='about-bar trigger5 animate fadeIn'></div>
+      <span className='about-message trigger5 animate fadeIn'>
         Welcome to the University of Washington Backgammon Club - 
         a vibrant community where students of all skill levels come 
         together to enjoy the strategic excitement of backgammon. Join 
         us for inclusive play, friendly competition, and a shared 
         passion for the game!</span>
-      <div className='about2'></div> 
-      <span className='statement-text trigger4 animate fadeIn'>
+      <div className='about2 trigger4 animate fadeLeftSpecial'></div> 
+      <span className='statement-text trigger5 animate fadeIn'>
         We are Washington's Premier Backgammon Society for
         casual & competitive Backgammon
       </span>
